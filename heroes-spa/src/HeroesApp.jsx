c07975af1bae1019216f7e779ec6e843353bf81b
Santiago <santic93.sc@gmail.com>
2024-05-29 11:57:15 -0300

    first commit

---
+++ heroes-spa/src/HeroesApp.jsx
@@ -0,0 +1,11 @@
+import AppRouter from './router/AppRouter';
+
+const HeroesApp = () => {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default HeroesApp;
---
+++ heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { MarvelPage, DcPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
+
+const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='marvel' element={<MarvelPage />} />
+        <Route path='dc' element={<DcPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='/' element={<Navigate to='/marvel' />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
