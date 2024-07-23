c00c2aa6ba133e1b70f2e76fa523b26552990be6
Santiago <santic93.sc@gmail.com>
2024-07-23 14:38:59 -0300

    privated router

--- heroes-spa/src/auth/pages/LoginPage.jsx
+++ heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,22 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+  const [name, setName] = useState();
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogin = () => {
     const lastPath = localStorage.getItem('lastPath') || '/';
-    login('Santiago Castro');
+    login(name);
     navigate(lastPath, {
       replace: true,
     });
   };
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
   return (
-    <div className='container mt5'>
+    <div className='container mt5 text-center'>
       <h1>Entrar</h1>
+
       <hr />
-      <button className='btn btn-primary' onClick={onLogin}>
+      <span>
+        <b>Ingrese su nombre: </b>
+      </span>
+      <input
+        type='text'
+        placeholder='Nombre y Apellido'
+        required={true}
+        onChange={handleChange}
+      />
+      <br />
+      <br />
+      <br />
+      <hr />
+      <button className='btn btn-primary' onClick={onLogin} disabled={!name} >
         Entrar
       </button>
     </div>
