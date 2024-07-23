da2caf0c7a5ea6c75b8404a48649733c5a2cbbc0
Santiago <santic93.sc@gmail.com>
2024-07-23 14:04:53 -0300

    logged

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
 
 const AppRouter = () => {
   return (
     <>
-     
       <Routes>
         <Route path='login' element={<LoginPage />} />
-        <Route path='/*' element={<HeroesRoutes />} />
+        <Route
+          path='/*'
+          element={
+            <PrivateRoute>
+              <Route path='/*' element={<HeroesRoutes />} />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
